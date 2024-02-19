69eb4c18b624d1fa6c80168a668f0c504aa627aa
aboAliAhmed <ahmedbeasar@gmail.com>
2024-02-19 11:18:30 +0200

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
+import Offers from './pages/Offers';
+import Dashboard from './pages/Dashboard';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact-us' element={<ContactUs/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+        <Route path='/Dahboard' element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+export default function s() {
+  return (
+    <div>Home</div>
+  )
+}
