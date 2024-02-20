90a8531e0cdc6b9d6f0e948cda9c67b2d033b75a
aboAliAhmed <ahmedbeasar@gmail.com>
2024-02-20 16:52:10 +0200

    create user model

---
+++ api/models/userModels.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "أدخل اسمك"],
+      unique: [true, "هذا الإسم موجود بالفعل, اختر اسماً أخر"],
+      trim: true,
+      maxlength: [20, "يجب ألا يزيد الإسم عن 20 حرفاً"],
+      minlength: [3, "يجب ألا يقل الإسم عن 3 حروف"],
+    },
+    email: {
+      type: String,
+      required: [true, "أدخل حساب المستخدم"],
+      unique: [true, "هذا الحساب موجود بالفعل, اختر حساباً أخر"],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "حدد كلمة المرور"],
+      maxlength: [20, "يجب ألا يزيد الإسم عن 20 حرفاً"],
+      minlength: [3, "يجب ألا تقل كلمة المرور عن 9 حروف"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "modrator", "admin"],
+      default: "user",
+    },
+    phone: {
+      type: Number,
+      maxlength: [11, "يجب ألا يزيد رقم الهاتف عن 11 رقماً"],
+      minlength: [11, "يجب ألا يقل رقم الهاتف عن 11 رقماً"],
+    },
+    country: {
+      type: String,
+      enum: ["egypt"],
+    },
+    government: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    streetAndAppartmentNo: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
