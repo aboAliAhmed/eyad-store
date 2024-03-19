31586bcf19ec80c791febf458eb3bfcf9ec63760
aboAliAhmed <ahmedbeasar@gmail.com>
2024-03-19 08:25:04 +0200

    add pre-installation

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import productRouter from "./routes/productRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import globalErrorHandler from "./controllers/errorController.js";
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -40,6 +44,12 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/order", orderRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(globalErrorHandler);
 
